edb6dfc6c1f76b5b109907e5c0d9f9ec09ae1e90
BJoseAntonio <bas.99jose@gmail.com>
2024-06-22 20:49:38 -0700

    HTML_CSS

--- Blog/gulpfile.js
+++ Blog/gulpfile.js
@@ -3,7 +3,7 @@ import * as darkSass from 'sass';
 import gulpsSass from 'gulp-sass';
 
 const sass = gulpsSass(darkSass)
-
+                            
 
 export function css(done){
     src('src/scss/app.scss', {sourcemaps:true})
